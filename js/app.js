56ddded39a6e393c10e5271055b0b042b1eb736a
alexliinc <alexli@indiana.edu>
2017-07-14 14:52:41 -0600

    redesign

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var winningCond = 
+var winningCond =
 [
 [1,2,3],[4,5,6],[7,8,9],
 [1,4,7],[2,5,8],[3,6,9],
@@ -8,53 +8,58 @@ var winningCond =
 var O = [];
 var X = [];
 var turns = 0;
+var winner = false;
 
 //playing the Game
 $playGame = function()
 {
-  if (!$(this).data('clicked'))
-  {
-    if (turns % 2 === 0)
+  if (!winner){
+    if (!$(this).data('clicked'))
     {
-      O.push(parseInt(this.id));
-      $(this).data('clicked', true);
-      $(this).html("X").css({"color":"blue", "text-align":"center","font-size":"60px"});
-      turns++;
+      if (turns % 2 === 0)
+      {
+        O.push(parseInt(this.id));
+        $(this).data('clicked', true);
+        $(this).html("O").css({"color":"blue", "text-align":"center","font-size":"100px"});
+        turns++;
+  		  $(this).addClass('avoidClick');
+      }
+      else
+      {
+        X.push(parseInt(this.id));
+        $(this).data('clicked', true);
+        $(this).html("X").css({"color":"red", "text-align":"center","font-size":"100px"});
+        turns++;
+        $(this).addClass('avoidClick');
+      }
     }
-    else
-    {
-      X.push(parseInt(this.id));
-      $(this).data('clicked', true);
-      $(this).html("O").css({"color":"red", "text-align":"center","font-size":"60px"});
-      turns++;
-    }
+     winnerWinner(O, 'O');
+     winnerWinner(X, 'X');
+    //  console.log(this.id);
+    //  console.log("turns are " + turns);
+    //  console.log("x is " + X);
+    //  console.log("o is " + O);
   }
-   winnerWinner(X);
-   winnerWinner(O);
-   console.log(this.id);
-   console.log("turns are " + turns);
-   console.log("x is " + X);
-   console.log("o is " + O);
 }
 
-function winnerWinner(X){
+function winnerWinner(array, letter){
 
     for( i = 0 ; i < winningCond.length; i++)
     {
         var currentWinCond = winningCond[i];
 
-        if (X.filter(function (elem){
+        if (array.filter(function (elem){
         return currentWinCond.indexOf(elem) > -1;
         }).length == currentWinCond.length)
         {
-            alert('WINNER');
+          winner = true;
+          alert('WINNER is ' + letter);
         }
-        else
-        {
-            console.log(false);
-        }
-
-    }   
+        // else
+        // {
+        //     console.log(false);
+        // }
+    }
 }
 
 $(".square").on("click", $playGame);
